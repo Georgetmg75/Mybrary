0f61f28ff505b710a043727d34b6b5f5c2d6aee1
george <mullergeorgey21@gmail.com>
2025-10-07 15:34:27 +0530

    updated enviro

--- server.js
+++ server.js
@@ -20,11 +20,16 @@ app.use(expressLayouts);
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
+
 const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
+
+
+
+
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
